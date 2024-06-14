831a9e383745af4a721826dc8a8b5de39c1d0eb4
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-14 14:44:27 +0200

    added carousel

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,70 @@
-// Importo lo style del main, l'array dei posts e la Post Card
+// Importo lo style del main, l'array dei posts, la Post Card, il carosello, lo useState ed i bottoni del carosello
 import PostCard from '../Card/PostCard';
 import mainStyle from './Main.module.css';
 import posts from '../../data/posts.js';
 import carousel from '../../data/carousel.js';
+import CarouselPost from '../CarouselPost/CarouselPost.jsx';
+import { useState } from 'react';
+import {
+    FaArrowAltCircleLeft as Prev,
+    FaArrowAltCircleRight as Next
+} from "react-icons/fa";
 
 const Main = () => {
 
+    const [activePost, setActivePost] = useState(0);
+
     const postsCarousel = carousel;
+
+    const prevClick = () => {
+        setActivePost(currentIndex =>
+            currentIndex === 0 ? postsCarousel.length - 1 : currentIndex - 1
+        );
+    }
+
+    const nextClick = () => {
+        setActivePost(currentIndex =>
+            currentIndex === postsCarousel.length - 1 ? 0 : currentIndex + 1
+        );
+    }
 
     return (
         <>
             <main className={mainStyle.mainSec}>
+
+                {/* Carosello dei Posts */}
+
+                <div className={mainStyle.carousel}>
+                    <button className={mainStyle.btn}>
+                        <Prev
+                            onClick={prevClick}
+                        />
+                    </button>
+
+                    {postsCarousel.map((pc, i) => (
+                        <CarouselPost
+                            key={pc.id}
+                            title={pc.title}
+                            image={pc.image}
+                            isActive={activePost === i}
+                        />
+                    ))}
+
+                    <button className={mainStyle.btn}>
+                        <Next
+                            onClick={nextClick}
+                        />
+                    </button>
+                </div>
+
+
+
+
+
+
+
+
+
                 {/* Genero in modo dinamico i Posts */}
                 {posts.map(p => (
                     p.published === true &&
