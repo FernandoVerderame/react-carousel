3bc7b701ced3654af5ac7abda40b9baa4403b51b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-14 17:09:36 +0200

    added thumbnails style

--- src/components/CarouselThumbnails/CarouselThumbnails.jsx
+++ src/components/CarouselThumbnails/CarouselThumbnails.jsx
@@ -1,11 +1,11 @@
 // Importo lo style dei thumbnails
 import carouselThumbnailsStyle from './CarouselThumbnails.module.css';
 
-const CarouselThumbnails = ({ image, onThumbnailClick }) => {
+const CarouselThumbnails = ({ image, isActive, onThumbnailClick }) => {
     return (
         <>
             <figure onClick={onThumbnailClick}>
-                <img src={image} alt='thumbnail-image' className={carouselThumbnailsStyle.img} />
+                <img src={image} alt='thumbnail-image' className={`${carouselThumbnailsStyle.img} ${isActive ? carouselThumbnailsStyle.active : ''}`} />
             </figure>
         </>
     );
